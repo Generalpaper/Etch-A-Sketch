33a1fed617c90983d3157c7eb9043d434aa0b157
Generalpaper <jethrojuan1@gmail.com>
2023-04-05 12:24:02 +0800

    finish all task requirements

--- script.js
+++ script.js
@@ -20,27 +20,25 @@ function createCanvas(noOfSquares) {
             let square = document.createElement("div");
             square.classList.add("square");
             square.addEventListener('mouseenter', () => {
-                let rgb1;
-                let rgb1pc10;
-                let rgb2;
-                let rgb2pc10;
-                let rgb3;
-                let rgb3pc10;
                 if (clicked % 2 == 0 && !square.style.backgroundColor ) {
-                    rgb1 = randomNumber(255);
-                    rgb1pc10 = rgb1 / 10;
-                    rgb2 = randomNumber(255);
-                    rgb2pc10 = rgb2 / 10;
-                    rgb3 = randomNumber(255);
-                    rgb3pc10 = rgb3 / 10;
+                    let rgb1 = randomNumber(255);
+                    let rgb1pc10 = rgb1 / 10;
+                    let rgb2 = randomNumber(255);
+                    let rgb2pc10 = rgb2 / 10;
+                    let rgb3 = randomNumber(255);
+                    let rgb3pc10 = rgb3 / 10;
+                    square.dataset.rgb1 = rgb1;
+                    square.dataset.rgb1pc10 = rgb1pc10;
+                    square.dataset.rgb2 = rgb2;
+                    square.dataset.rgb2pc10 = rgb2pc10;
+                    square.dataset.rgb3 = rgb3;
+                    square.dataset.rgb3pc10 = rgb3pc10;
                     square.style.backgroundColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
-                } else if (clicked % 2 == 0 && square.style.backgroundColor) {
-                    console.log(rgb1)
-                    rgb1 = rgb1 - rgb1pc10;
-                    console.log(rgb1)
-                    rgb2 = rgb2 - rgb2pc10;
-                    rgb3 = rgb3 - rgb3pc10;
-                    square.style.backgroundColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
+                } else if (clicked % 2 == 0 && square.style.backgroundColor && square.dataset.rgb1 > 0) {
+                    square.dataset.rgb1 = square.dataset.rgb1 - square.dataset.rgb1pc10;
+                    square.dataset.rgb2 = square.dataset.rgb2 - square.dataset.rgb2pc10;
+                    square.dataset.rgb3 = square.dataset.rgb3 - square.dataset.rgb3pc10;
+                    square.style.backgroundColor = `rgb(${square.dataset.rgb1}, ${square.dataset.rgb2}, ${square.dataset.rgb3})`;
                 }
             });
             square.style.cssText = `width: ${lengthOfEach}px;`;
@@ -69,3 +67,4 @@ function squarechange() {
     container.removeChild(canvas);
     createCanvas(noOfSquares);
 }
+// https://stackoverflow.com/questions/11286661/set-custom-attribute-using-javascript
